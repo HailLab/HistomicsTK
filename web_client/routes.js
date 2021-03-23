7d0fafe65d071574f7fbcf503e3ee97dc080ce6d
Joseph Coco <joseph.r.coco@vumc.org>
2021-03-23 00:08:15 +0000

    Got /histomicstk directing to first image, but still displays errors.

--- web_client/routes.js
+++ web_client/routes.js
@@ -9,11 +9,12 @@ function bindRoutes() {
     Router.route('', 'index', function () {
         var firstImage = '5f0f2da097cefa564329547b';
         var folderId = '5f0dc45cc9f8c18253ae949b';
-        var first = restRequest({
+        restRequest({
             url: `item/5f0dc45cc9f8c18253ae949b/first_image?folder=5f0dc45cc9f8c18253ae949b`
+        }).done(function (first) {
+            firstImage = typeof first !== 'undefined' ? first._id : firstImage;
+            events.trigger('g:navigateTo', ImageView, {'modelId': firstImage});
         });
-        firstImage = typeof first !== 'undefined' ? first._id : firstImage;
-        events.trigger('g:navigateTo', ImageView, {'modelId': firstImage});
     });
     return Router;
 }
